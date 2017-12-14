305f0ad7f6b3b0ea11be5d0a8ad98125e4220ca7
Bunlong <bulong.van@gmail.com>
2017-12-14 15:46:39 +0700

    done backend

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,92 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const debug = require('debug')('backend:server');
+const logger = require('morgan');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const http = require("http");
+const socketIo = require("socket.io");
+const Moniker = require('moniker');
 
-var app = express();
+const index = require('./routes/index');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const app = express();
+const port = normalizePort(process.env.PORT || '8000');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('port', port);
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+const nameGen = Moniker.generator([Moniker.adjective, Moniker.noun]);
+
+// User
+let getUser = (id) => {
+  let nickname = _users.get(id);
+
+  if (!nickname) {
+    nickname  = nameGen.choose();
+    let count = 0;
+
+    for (let name of _users.values()) {
+      if (name === nickname) {
+        count += 1;
+      }
+    }
+
+    if (count !== 0) {
+      nickname += ` (${count})`;
+    }
+
+    _users.set(id, nickname);
+  }
+
+  return nickname;
+};
+// ===================================
+
+// connection event
+io.on('connection', function(socket) {  
+  console.log("A user is connected");
+  socket.username = getUser(socket.id);
+  
+  socket.emit('notify user', socket.username);
+  socket.broadcast.emit('user connected', socket.username);
+
+  // disconnect event
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user disconnected', socket.username);
+  });
+
+  // chat message event
+  socket.on('chat message', (params) => {
+    let timestamp = (new Date()).toISOString();
+
+    io.emit('chat message', {
+      nickname: socket.username,
+      message: params.message,
+      time: timestamp
+    });
+  });
+
+  // user typing event
+  socket.on('user typing', (isTyping) => {
+    if (isTyping === true) {
+      socket.broadcast.emit('user typing', {
+        nickname: socket.username,
+        isTyping: true
+      });
+    } else {
+      socket.broadcast.emit('user typing', {
+        nickname: socket.username,
+        isTyping: false
+      });
+    }
+  });
+});
+// ===================================
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +106,81 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+server.listen(port, () => console.log(`Listening on port ${port}`));
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+// var favicon = require('serve-favicon');
+// var path = require('path');
+/**
+ * view engine setup
+ */
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+/**
+ * =================
+ */
